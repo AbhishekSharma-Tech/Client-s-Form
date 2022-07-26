1ad9a562d610d2e765bfca1c4679380884a3d765
Abhishek Sharma <76276127+AbhishekSharma-Tech@users.noreply.github.com>
2022-07-26 10:39:54 +0530

    Update view-project.php

--- view-project.php
+++ view-project.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Client's Form | View Projects</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    
+    <!-- CSS CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
     <style type="text/css">
         .bs-example{
         margin: 20px;
@@ -22,7 +22,7 @@
 </head>
 <body>
 <div class="bs-example">
-    <div class="container">
+    <div class="container card-view">
         <div class="row">
             <div class="col-md-12">
                 <div class="page-header clearfix">
@@ -35,22 +35,22 @@
                 <?php
                     if (mysqli_num_rows($result) > 0) {
                 ?>
-                    <table class='table table-bordered table-striped'>
-                        <tr>
-                            <th>Topic</th>
-                            <th>Number of Words</th>
-                            <th>Instructions</th>
-                            <th>Scheduled Delivery Date</th>
+                    <table class='table table-bordered table-striped' style="border: 3px solid white;">
+                        <tr style="border: 3px solid white;">
+                            <th style="border: 2px solid white; color: #e3e3e3; text-align: center; font-size: 20px">Topic</th>
+                            <th style="border: 2px solid white; color: #e3e3e3; text-align: center; font-size: 20px">Number of Words</th>
+                            <th style="border: 2px solid white; color: #e3e3e3; text-align: center; font-size: 20px">Instructions</th>
+                            <th style="border: 2px solid white; color: #e3e3e3; text-align: center; font-size: 20px">Scheduled Delivery Date</th>
                         </tr>
                         <?php
                             $i=0;
                             while($row = mysqli_fetch_array($result)) {
                                 ?>
                                 <tr>
-                                    <td><?php echo $row['Topic']; ?></td>
-                                    <td><?php echo $row["Words"]; ?></td>
-                                    <td><?php echo $row["Descriptions"]; ?></td>
-                                    <td><?php echo $row["Delivery_Date"]; ?></td>
+                                    <td style="border: 2px solid white; color: #e7eaf6; font-size: 15px"><?php echo $row['Topic']; ?></td>
+                                    <td style="border: 2px solid white; color: #e7eaf6; font-size: 15px"><?php echo $row["Words"]; ?></td>
+                                    <td style="border: 2px solid white; color: #e7eaf6; font-size: 15px"><?php echo $row["Descriptions"]; ?></td>
+                                    <td style="border: 2px solid white; color: #e7eaf6; font-size: 15px"><?php echo $row["Delivery_Date"]; ?></td>
                                 </tr>
                                 <?php
                                     $i++;
@@ -60,7 +60,7 @@
                     <?php
                 }
                 else{
-                    echo "No result found";
+                    echo "<p style = 'color: white; font-size: 20px; font-weight: 500;'>No Record Found</p>";
                 }
                 ?>
             </div>
